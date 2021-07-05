b2263228bc335e1536c41ba2df20fc19446d918b
Ahmed Maged <ahmedmaged20188@gmail.com>
2021-07-06 01:45:37 +0200

    badges update

--- sections/badges/index.js
+++ sections/badges/index.js
@@ -12,20 +12,20 @@ const Badges = () => {
         <EmojiHappyIcon className=" inline-flex w-20 mx-1  text-red-400" />
         <p className="text-3xl text-black ">Happy</p>
         <p className="text-xl text-gray-600">
-          makes you say sheeshh after every trip
+          Makes you satsfied with our quality choices
         </p>
       </div>
       <div className="text-4xl text-black space-y-3 content-center justify-start text-center">
         <CalculatorIcon className=" inline-flex w-20 mx-1  text-red-400" />
         <p className="text-3xl text-black ">Calculated</p>
-        <p className="text-xl text-gray-600">
-          Will calculate trips for your broke ass
-        </p>
+        <p className="text-xl text-gray-600">Calculated and accurate reviews</p>
       </div>
       <div className="text-4xl text-black space-y-3 justify-center text-center">
         <BadgeCheckIcon className=" inline-flex w-20 mx-1  text-red-400" />
         <p className="text-3xl text-black ">Top rated</p>
-        <p className="text-xl text-gray-600">We only suggest dope places</p>
+        <p className="text-xl text-gray-600">
+          We represent top quality experience
+        </p>
       </div>
     </div>
   );
